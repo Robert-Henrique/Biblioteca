625e51bf7b123e5a3e9526f140c89ed83ef7d00c
RobertCampos <roberthenrique2010@hotmail.com>
2016-05-31 22:19:30 -0300

    Correção de bugs

---
+++ Biblioteca/app/Modulo.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module("app", ['ngRoute', 'ui.bootstrap']);
+
+app.config(function ($routeProvider, $locationProvider) {
+
+    // remove o # da url
+    //$locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/Templates/Livro/Index.html',
+            controller: 'detalhesController'
+        })
+        .when('/livros', {
+            templateUrl: 'app/Templates/Livro/Index.html',
+            controller: 'detalhesController'
+        })
+        .when('/livros/detalhes', {
+            templateUrl: 'app/Templates/Livro/Detalhes.html',
+            controller: 'detalhesController'
+        })
+        .when('/livros/detalhes/:id', {
+            templateUrl: 'app/Templates/Livro/Detalhes.html',
+            controller: 'detalhesController'
+        }).otherwise({
+            redirectTo: '/'
+        });
+});
